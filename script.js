8710b0feceb9baeb1392de50da005eebdd8b049c
Gabriel Francisco Pedro Alexandre <cap1barretim@gmail.com>
2023-06-09 19:39:01 -0300

    att

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const navMenu   = document.querySelector(".nav-menu");
 btnRespon.addEventListener("click", () => {
     btnRespon.classList.toggle('active');
     navMenu.classList.toggle('active');
-})
+});
+
+const mudar = document.querySelector(".mod");
+var acao = document.querySelector(".mud");
+
+
+acao.addEventListener("click", () => {
+    mudar.classList.toggle('mudar');
+
+});
